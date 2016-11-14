c5b73c9c356d8fb061f75a458dcc654b27cd5ecd
Steven <smvandeventer@gmail.com>
2016-11-13 19:14:55 -0800

    teamview

--- views/team/team.js
+++ views/team/team.js
@@ -1,65 +1,65 @@
 'Use Strict';
 angular.module('App').controller('teamController', function (Auth, $state, $scope, StepLog, Admin, user) { 
 
-  // console.log(Admin)
+  console.log(Admin)
 
-  // Admin.getUsersFromTeam(user.team).$loaded().then(function(users){
-  //   $scope.users = users.sort(function(a, b){
-  //     if (a.firstname.toUpperCase() > b.firstname.toUpperCase()) {
-  //       return 1;
-  //     }
-  //     if (a.firstname.toUpperCase() < b.firstname.toUpperCase()) {
-  //       return -1;
-  //     }
-  //     // a must be equal to b
-  //     return 0;
-  //   })
-  // });
+  Admin.getUsersFromTeam(user.team).$loaded().then(function(users){
+    $scope.users = users.sort(function(a, b){
+      if (a.firstname.toUpperCase() > b.firstname.toUpperCase()) {
+        return 1;
+      }
+      if (a.firstname.toUpperCase() < b.firstname.toUpperCase()) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    })
+  });
 
 
-  // $scope.query = {};
+  $scope.query = {};
 
  
-  // $scope.onUserSelect = function(){
-  //   console.log($scope.query.user)
-  //   $scope.users.forEach(function(user, i){
-  //     if(angular.equals($scope.query.user, user)){
-  //       $scope.userIndex = i;
+  $scope.onUserSelect = function(){
+    console.log($scope.query.user)
+    $scope.users.forEach(function(user, i){
+      if(angular.equals($scope.query.user, user)){
+        $scope.userIndex = i;
 
-  //     }
-  //   });
+      }
+    });
 
-  //   $scope.entries = StepLog.getEntries($scope.query.user.id);
+    $scope.entries = StepLog.getEntries($scope.query.user.id);
 
-  //   $scope.total = 0;
+    $scope.total = 0;
 
-  //   $scope.entries.$loaded().then(function(){
-  //     $scope.entries.forEach(function(entry){
-  //       $scope.total += entry.steps;
-  //     });
-  //   });
+    $scope.entries.$loaded().then(function(){
+      $scope.entries.forEach(function(entry){
+        $scope.total += entry.steps;
+      });
+    });
 
-  //   console.log($scope.userIndex);
-  // }
+    console.log($scope.userIndex);
+  }
 
-  // $scope.selectNextUser = function(){
+  $scope.selectNextUser = function(){
     
-  //   if($scope.users.length > $scope.userIndex + 1){
-  //     $scope.query.user = $scope.users[$scope.userIndex + 1];
-  //   } else {
-  //     $scope.query.user = $scope.users[0];
-  //   }
+    if($scope.users.length > $scope.userIndex + 1){
+      $scope.query.user = $scope.users[$scope.userIndex + 1];
+    } else {
+      $scope.query.user = $scope.users[0];
+    }
 
-  //   $scope.onUserSelect();
-  // }
+    $scope.onUserSelect();
+  }
 
-  // $scope.selectPreviousUser = function(){
-  //   if(0 < $scope.userIndex){
-  //     $scope.query.user = $scope.users[$scope.userIndex - 1];
-  //   } else {
-  //     $scope.query.user = $scope.users[$scope.users.length - 1];
-  //   }
+  $scope.selectPreviousUser = function(){
+    if(0 < $scope.userIndex){
+      $scope.query.user = $scope.users[$scope.userIndex - 1];
+    } else {
+      $scope.query.user = $scope.users[$scope.users.length - 1];
+    }
 
-  //   $scope.onUserSelect();
-  // }
+    $scope.onUserSelect();
+  }
 });
