3ed40fd3d0ca042100362576743d6a0c8f089a76
Steven <smvandeventer@gmail.com>
2017-08-14 09:31:15 -0700

    2017 spell check

--- views/register/register.js
+++ views/register/register.js
@@ -18,7 +18,7 @@ angular.module('App').controller('registerController', function ($scope, $state,
   };
 
  
-    $scope.ous = ['Audit Services','Health & Safety','Customer Serive','Corp Comm','EIX','Eithics & Compliance', 'Finance','Government Affairs', 'Human Resources', 'Legal', 'Environmental Services', 'EPM/Generation', 'Reg Affairs & SIPP', 'Supply Chain','Transportation Services']
+    $scope.ous = ['Audit Services','Health & Safety','Customer Service','Corp Comm','EIX','Ethics & Compliance', 'Finance','Government Affairs', 'Human Resources', 'Legal', 'Environmental Services', 'EPM/Generation', 'Reg Affairs & SIPP', 'Supply Chain','Transportation Services']
       
 
 
