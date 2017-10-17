33cd370bb26c2f4d361f953bad6a1b3029f569ed
Steven <smvandeventer@gmail.com>
2017-10-16 22:13:11 -0700

    1weekleft

--- views/leaderboard/leaderboard.js
+++ views/leaderboard/leaderboard.js
@@ -4,82 +4,82 @@ angular.module('App').controller('leaderboardController', function (Auth, $state
    
   // $scope.ous = ['Audit Services','CEHS','Corp Comm','EIX','Eithics & Compliance', 'Finance','Government Affairs', 'Human Resources', 'Legal', 'EPM/Generation', 'Reg Affairs & SIPP', 'Edison Material Supply','Transportation Services'];
 
-  $scope.ous = ['Audit Services','Health & Safety','Customer Service','Corp Comm','EIX','Ethics & Compliance', 'Finance','Information Technology', 'Human Resources', 'Legal', 'Environmental Services', 'EPM/Generation', 'Reg Affairs & SIPP', 'Supply Chain','Transportation Services'];
+  // $scope.ous = ['Audit Services','Health & Safety','Customer Service','Corp Comm','EIX','Ethics & Compliance', 'Finance','Information Technology', 'Human Resources', 'Legal', 'Environmental Services', 'EPM/Generation', 'Reg Affairs & SIPP', 'Supply Chain','Transportation Services'];
 
-  $scope.avgs = [];
+  // $scope.avgs = [];
 
   
-  angular.forEach($scope.avgs, function(avg){
-    $scope.ous.push(avg.ou);
-  })
+  // angular.forEach($scope.avgs, function(avg){
+  //   $scope.ous.push(avg.ou);
+  // })
 
-  $scope.user = user;
+  // $scope.user = user;
 
-   Utils.show();
+  //  Utils.show();
 
-  var promises = []
+  // var promises = []
 
-  $scope.ous.forEach(function(ou){
+  // $scope.ous.forEach(function(ou){
 
-    promises.push(Admin.getUsersFromOu(ou).$loaded().then(function(profiles){
-      var numOuUsers = profiles.length;
+  //   promises.push(Admin.getUsersFromOu(ou).$loaded().then(function(profiles){
+  //     var numOuUsers = profiles.length;
 
-      var ouTotal = 0;
-      var numDupes = 0;
+  //     var ouTotal = 0;
+  //     var numDupes = 0;
 
-      profiles.forEach(function(profile){
-        if(profile.steps){
-          ouTotal += profile.steps;
-        } else {
-          numDupes += 1;
-        }
-      });
+  //     profiles.forEach(function(profile){
+  //       if(profile.steps){
+  //         ouTotal += profile.steps;
+  //       } else {
+  //         numDupes += 1;
+  //       }
+  //     });
 
-       $scope.avgs.push({ou: ou, ouAvg: ouTotal/(numOuUsers - numDupes)});
+  //      $scope.avgs.push({ou: ou, ouAvg: ouTotal/(numOuUsers - numDupes)});
 
-      if(user.ou == ou){
-        $scope.ouProfiles = profiles;
-        var groupedTeams = {};
+  //     if(user.ou == ou){
+  //       $scope.ouProfiles = profiles;
+  //       var groupedTeams = {};
 
-        profiles.forEach(function(profile){
-          if(profile.team === 'Empty' || !profile.steps){
-            return;
-          }
+  //       profiles.forEach(function(profile){
+  //         if(profile.team === 'Empty' || !profile.steps){
+  //           return;
+  //         }
 
-          if(!groupedTeams[profile.team]){
-            groupedTeams[profile.team] = [];
-          }
+  //         if(!groupedTeams[profile.team]){
+  //           groupedTeams[profile.team] = [];
+  //         }
 
-          groupedTeams[profile.team].push(profile);
-        });
+  //         groupedTeams[profile.team].push(profile);
+  //       });
 
-        $scope.teamTotals = [];
-        console.log(groupedTeams);
+  //       $scope.teamTotals = [];
+  //       console.log(groupedTeams);
 
-        angular.forEach(groupedTeams, function(profiles, team){
-          var total = 0;
+  //       angular.forEach(groupedTeams, function(profiles, team){
+  //         var total = 0;
 
-          angular.forEach(profiles, function(profile){
-            total += profile.steps;
-          });
+  //         angular.forEach(profiles, function(profile){
+  //           total += profile.steps;
+  //         });
 
-          $scope.teamTotals.push({team: team, total: total, avg: total/profiles.length});
-        });
+  //         $scope.teamTotals.push({team: team, total: total, avg: total/profiles.length});
+  //       });
 
-      }
-    }));
+  //     }
+  //   }));
 
-  });
-
-
-  promises.push(Auth.getTopProfiles().$loaded().then(function(profiles){
-    return $scope.profiles = profiles;
-  }));
+  // });
 
 
-  $q.all(promises).then(function(){
-    Utils.hide()
-  })
+  // promises.push(Auth.getTopProfiles().$loaded().then(function(profiles){
+  //   return $scope.profiles = profiles;
+  // }));
+
+
+  // $q.all(promises).then(function(){
+  //   Utils.hide()
+  // })
 
   
 
